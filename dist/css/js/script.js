2af3d70207e71357f01923efa154109775264b92
widi1502 <130680734+widi1502@users.noreply.github.com>
2023-04-13 22:03:06 +0700

    Add files via upload

---
+++ dist/css/js/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    menu.classList.toggle("menu-active");
+});
+
+window.addEventListener('scroll', () => {
+    hamburger.classList.remove('is-active');
+    menu.classList.remove("menu-active");
+});
